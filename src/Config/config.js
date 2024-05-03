ee073dd9281ced4fcc8eeda307fff76ed53193c5
Ronak <vivekn@bravishma.com>
2024-05-03 05:16:02 +0000

    config changed

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    cobrowseOrgSignupUrl: 'http://localhost:3011/cobrowse/orgs',
-    cobrowseUserLoginUrl: 'http://localhost:3011/cobrowse/user/login',
+    cobrowseOrgSignupUrl: 'https://rahul.lab.bravishma.com/cobrowse/orgs',
+    cobrowseUserLoginUrl: 'https://rahul.lab.bravishma.com/cobrowse/user/login',
 };
 
 export default config;
